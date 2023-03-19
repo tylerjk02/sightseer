7aaa52c32bee731eceb0e9cf5a0557a2d4da6c99
Tyler Kraft <tylerjkraft2002@gmail.com>
2023-03-19 13:34:34 -0400

    better routing, more country details

--- src/routes/countries/[slug]/+page.svelte
+++ src/routes/countries/[slug]/+page.svelte
@@ -1,34 +1,91 @@
-
 <script lang="ts">
+  import lookup from "country-code-lookup";
+
   export let data;
   const { country } = data;
   const item = country[0];
 
-  const { name, region } = item;
-  console.log(item);
+  const {
+    name,
+    region,
+    coatOfArms,
+    flags,
+    area,
+    capital,
+    languages,
+    population,
+    independent,
+    landlocked,
+    borders,
+  } = item;
+
+  let borderList: any[] = [];
+  for (let i = 0; i < borders.length; i++) {
+    borderList.push(lookup.byIso(borders[i]));
+  }
+  
 </script>
 
 <div class="main">
   <div class="region">
     <div class="trace-back">
-      <a href="/">home</a>/<a href="/continents/{region.toLowerCase()}">{region.toLowerCase()}</a>/<p>{name.common.toLowerCase()}</p>
+      <a href="/">home</a>/<a href="/continents/{region.toLowerCase()}"
+        >{region.toLowerCase()}</a
+      >/
+      <p>{name.common.toLowerCase()}</p>
     </div>
   </div>
   <div class="country">
-
-    <h1>{name.common}</h1>
+    <div class="country-political-images">
+      {#if coatOfArms.hasOwnProperty("svg")}
+        <img class="country-coa" src={coatOfArms.svg} alt="Coat of Arms" />
+      {/if}
+      <img class="country-flag" src={flags.svg} alt={flags.alt} />
+    </div>
+    <div class="country-info">
+      <div class="country-top">
+        <h1>{name.common}</h1>
+        <p>{name.official}</p>
+      </div>
+      <div class="country-mid">
+        <h2>Geography:</h2>
+        <p>{area.toLocaleString()}km<sup>2</sup></p>
+        {#if borders}
+          <p>Borders:</p> 
+          
+          {#each borderList as border}
+            <div class="border-country">
+              <p>{border.country}</p>
+            </div>
+          {/each}
+        {/if}
+        <h2>Demographics:</h2>
+        <p>Population: {population.toLocaleString()}</p>
+      </div>
+    </div>
   </div>
-  
 </div>
 
 <style>
   .trace-back {
-    display:flex;
+    display: flex;
     gap: 1px;
   }
   .country {
     background: #e3e3e3;
     padding: 1rem;
     margin: 5px;
+  }
+  .country-flag {
+    width: 70px;
+  }
+  .country-coa {
+    width: 65px;
+  }
+
+  .country-political-images {
+    display: flex;
+    gap: 10px;
+    align-items: center;
   }
 </style>
