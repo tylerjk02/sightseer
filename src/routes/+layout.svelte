dfc9f0c60e47e67050720216ee5611734d86efd0
Tyler Kraft <tylerjkraft2002@gmail.com>
2023-03-25 19:22:05 -0400

    vercel inject

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
   import "../app.css";
-  import Header from '$lib/Header.svelte';
+  import Header from "$lib/Header.svelte";
+  import { dev } from "$app/environment";
+  import { inject } from "@vercel/analytics";
+
+  inject({ mode: dev ? "development" : "production" });
 </script>
 
 <Header />
 
-
-<slot></slot>
+<slot />
