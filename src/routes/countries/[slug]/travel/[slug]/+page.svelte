796ed3fdbb264fcc71471fc891b3bdc15ca161af
Tyler Kraft <tylerjkraft2002@gmail.com>
2023-03-30 16:27:52 -0400

    hotfix

--- src/routes/countries/[slug]/travel/[slug]/+page.svelte
+++ src/routes/countries/[slug]/travel/[slug]/+page.svelte
@@ -3,12 +3,15 @@
   const { citySlug, countrySlug } = data;
   const hotelData: any[] = JSON.parse(data.hotelData!);
   const hotelList: any = [];
-
-  hotelData.forEach((e: any) => {
-    if (e.name !== "") {
-      hotelList.push(e);
-    }
-  });
+  const fetchHotelData = async () => {
+    setTimeout(() => {
+      hotelData.forEach((e: any) => {
+        if (e.name !== "") {
+          hotelList.push(e);
+        }
+      });
+    });
+  };
 
   console.log(hotelList);
 </script>
@@ -18,18 +21,21 @@
     <a href="/countries/{countrySlug}/travel">Back</a>
   </div>
   <h1>Travel in {citySlug}, {countrySlug}</h1>
-
-  {#if hotelList.length !== 0}
-    <div class="hotels">
-      {#each hotelList as { name, area, addons }}
-        <h3>{name}</h3>
-        <p>{area}</p>
-        {#each addons as addon}
-          <p>{addon}</p>
+  {#await fetchHotelData}
+    <p>...loading</p>
+  {:then}
+    {#if hotelList.length !== 0}
+      <div class="hotels">
+        {#each hotelList as { name, area, addons }}
+          <h3>{name}</h3>
+          <p>{area}</p>
+          {#each addons as addon}
+            <p>{addon}</p>
+          {/each}
         {/each}
-      {/each}
-    </div>
-  {/if}
+      </div>
+    {/if}
+  {/await}
 </div>
 
 <style>
