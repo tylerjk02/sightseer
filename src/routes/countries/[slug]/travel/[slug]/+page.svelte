1b598342ccf58528c6208647c70f7672d9412a4e
Tyler Kraft <tylerjkraft2002@gmail.com>
2023-03-30 17:39:19 -0400

    fixed travel routes (again)

--- src/routes/countries/[slug]/travel/[slug]/+page.svelte
+++ src/routes/countries/[slug]/travel/[slug]/+page.svelte
@@ -1,19 +1,55 @@
 <script lang="ts">
+  import axios from "axios";
+  import cheerio from "cheerio";
+
   export let data;
   const { citySlug, countrySlug } = data;
-  const hotelData: any[] = JSON.parse(data.hotelData!);
-  const hotelList: any = [];
-  const fetchHotelData = async () => {
-    setTimeout(() => {
-      hotelData.forEach((e: any) => {
-        if (e.name !== "") {
-          hotelList.push(e);
+  let hotelObjList: any[] = [];
+
+  const getHotelData = async (city: string, country: string) => {
+    try {
+      const url = `https://www.hotels.com/Hotel-Search?destination=${city}%2C%20${country}`;
+      const { data } = await axios({
+        method: "GET",
+        url: url,
+      });
+
+      const $ = cheerio.load(data);
+      const hotelCard = ".uitk-spacing-margin-blockstart-three";
+
+      $(hotelCard).each((parentIdx: any, parentElem: any) => {
+        const hotelObject: any = {};
+        if (parentIdx < 30) {
+          $(parentElem)
+            .children()
+            .each((childIdx: any, childElem: any) => {
+              const hotelName = $(childElem).find(".uitk-heading-5").text();
+              const hotelArea = $(childElem)
+                .find(".uitk-text-spacing-half")
+                .text();
+              const addonList: any = [];
+              const hotelAddons = $(childElem)
+                .find(".uitk-layout-flex > .truncate")
+                .each((idx: any, elem: any) => {
+                  const addonContent = $(elem).text();
+                  addonList.push(addonContent);
+                });
+              hotelObject.name = hotelName;
+              hotelObject.area = hotelArea;
+              hotelObject.addons = addonList;
+            });
+
+          hotelObjList.push(hotelObject);
+          hotelObjList = hotelObjList;
         }
       });
-    });
+      return hotelObjList;
+    } catch (err) {
+      console.error(err);
+    }
   };
 
-  console.log(hotelList);
+  getHotelData(citySlug, countrySlug);
 </script>
 
 <div class="travel">
@@ -21,21 +57,18 @@
     <a href="/countries/{countrySlug}/travel">Back</a>
   </div>
   <h1>Travel in {citySlug}, {countrySlug}</h1>
-  {#await fetchHotelData}
-    <p>...loading</p>
-  {:then}
-    {#if hotelList.length !== 0}
-      <div class="hotels">
-        {#each hotelList as { name, area, addons }}
-          <h3>{name}</h3>
-          <p>{area}</p>
-          {#each addons as addon}
-            <p>{addon}</p>
-          {/each}
+
+  {#if hotelObjList.length !== 0}
+    <div class="hotels">
+      {#each hotelObjList as { name, area, addons }}
+        <h3>{name}</h3>
+        <p>{area}</p>
+        {#each addons as addon}
+          <p>{addon}</p>
         {/each}
-      </div>
-    {/if}
-  {/await}
+      {/each}
+    </div>
+  {/if}
 </div>
 
 <style>
